d793e9b882ba8cf1b8f66878858b3f4e9a51e887
allanw <al.whatmough@gmail.com>
2020-08-12 19:09:05 +1000

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-nomie",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Al Whatmough",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    install_requires=[
+        "singer-python>=5.0.12",
+        "requests",
+	"couchdb"
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-nomie=tap_nomie:main
+    """,
+    packages=["tap_nomie"],
+    #package_data = {
+    #    "schemas": ["{{cookiecutter.package_name}}/schemas/*.json"]
+    #},
+    include_package_data=True,
+)
---
+++ tap_nomie/__init__.py
@@ -0,0 +1,36 @@
+import couchdb
+import singer
+import json
+
+REQUIRED_CONFIG_KEYS = ["endpoint", "api_key"]
+
+def main():
+
+  args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
+
+  config = args.config
+
+  db_full_url = 'https://' + config['endpoint'] + ':' + config['api_key'] + '@' + config['endpoint'] + '.cloudantnosqldb.appdomain.cloud'
+
+#  session = client.session()
+#  db_name = client.all_dbs()[0]
+#  nomie_db = client[db_name]
+#  import pdb;pdb.set_trace()
+
+  couch = couchdb.Server(db_full_url)
+
+  db = couch['nomie-db']
+
+  schema = {'type': 'object',
+    'properties':
+      {
+        '_id': {'type': 'integer'},
+      }}
+
+  singer.write_schema('books', schema, 'id')
+
+  for book in db:
+    if 'books' in book and not book.endswith('_last'):
+      data = db[book]['data']
+      singer.write_records('books', data)
+
